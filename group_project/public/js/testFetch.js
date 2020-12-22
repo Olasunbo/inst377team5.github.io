f01fe20c61052addf52db02d72ae77a0ce4dd97f
Huy360 <71339043+Huy360@users.noreply.github.com>
2020-12-21 22:46:14 -0500

    change

--- group_project/public/js/testFetch.js
+++ group_project/public/js/testFetch.js
@@ -1,1 +1,30 @@
+/* 
+* testFetch.js file that tests the fetch API function with PG country libraries JSON
+*/
 
+//PG country libraries JSON URL
+testFetchUrl = ""
+
+//array that will hold the JSON
+testArray = [];
+libraryAddressArray = [];
+
+//fetches JSON then pushes into array
+fetch(testFetchUrl)
+.then(blob => blob,json())
+.then(data => testArray.push(...data));
+
+//testFetchFunction fetches JSON fromURL
+function testFetchFunction()
+{
+    //tells user that libraries have been fetched
+    document.getElementById("fetchLibraries").innerHTML = "All Libraries Received!";
+    /*
+    * loops throught JSON array and pushes each location and address
+    * into library array
+    */ 
+   for(x in testArray)
+   {
+       libraryAddressArray.push(testArray[x]["location_1"]);
+   }
+}
